fb87682ac07f663045d9fb93011637c2d9e4e636
Moses Bartena Towet <121856163+4ourpixels@users.noreply.github.com>
2023-07-07 20:36:25 +0300

    2nd commit

--- mazeras/forms.py
+++ mazeras/forms.py
@@ -18,3 +18,27 @@ class StoneForm(forms.ModelForm):
             'price': forms.NumberInput(attrs={'class': 'form-control'}),
             'stock': forms.NumberInput(attrs={'class': 'form-control'}),
         }
+
+
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        fields = [
+            'name',
+            'description',
+            'location',
+            'stones',
+        ]
+
+        labels = {
+            'name': 'Name',
+            'description': 'Description',
+            'location': 'Location',
+            'stones': 'Stones',
+        }
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.TextInput(attrs={'class': 'form-control'}),
+            'location': forms.TextInput(attrs={'class': 'form-control'}),
+            'stones': forms.TextInput(attrs={'class': 'form-control'}),
+        }
