30317d9bac8e936391224198ef28bf70bae50ef3
Moses Bartena Towet <121856163+4ourpixels@users.noreply.github.com>
2023-07-08 10:58:45 +0300

    3rd commit

--- bvmazeras/settings.py
+++ bvmazeras/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-=zwg=ngix(k#y-ciy5k65d@g@p+elnomllv(=x7=)t9=ou3-^)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['blessedventures.pythonanywhere.com']
 
 
 # Application definition
